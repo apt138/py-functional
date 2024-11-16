db1f781e26fda088dcd88c4b7a32b212cd367b47
apt138 <ap.138@outlook.com>
2024-11-16 12:19:46 +0530

    filter

---
+++ filter.py
@@ -0,0 +1,59 @@
+"""
+Given two lists of equal length, one called reference
+containing binary values either 0 or 1 and another called 
+values containing strings.
+
+A function that returns a new list containing only those
+strings from the values list where the positionally corresponding
+value in the refreence list.
+
+Assumptions:
+1. the two input lists will always have equal length.
+2. the reference list will only contains 0  and 1s
+3. the values list will contains strings with length 1.
+"""
+
+import unittest
+
+reference: list[int] = [0, 1, 1, 0, 1, 0]
+values: list[str] = list("abcdef")
+
+
+def transform(ref: list[int], val: list[str]) -> list[str]:
+    filtered_ref_val = filter(lambda x: x[0], zip(ref, val))
+    filtered_val = map(lambda x: x[1], filtered_ref_val)
+    return list(filtered_val)
+
+
+class TestTransform(unittest.TestCase):
+    def test_sample_one(self):
+        ref = [0, 1]
+        val = ["a", "b"]
+        result = transform(ref, val)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result, ["b"])
+
+    def test_sample_two(self):
+        ref = [0, 0]
+        val = ["a", "b"]
+        result = transform(ref, val)
+        self.assertEqual(len(result), 0)
+        self.assertEqual(result, [])
+
+    def test_sample_three(self):
+        ref = [1, 1]
+        val = ["a", "b"]
+        result = transform(ref, val)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result, val)
+
+    def test_sample_four(self):
+        ref = []
+        val = []
+        result = transform(ref, val)
+        self.assertEqual(len(result), 0)
+        self.assertEqual(result, val)
+
+
+if __name__ == "__main__":
+    unittest.main()
